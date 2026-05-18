ee47a5fa96122abd36b533d7f740c2be4681ebf5
FunChaosSeeker68 <thebet5575@gmail.com>
2026-05-18 17:25:24 +0300

    ביצעתי דיבאגינג וגם הרצתי את הקוד

--- scripts/main.py
+++ scripts/main.py
@@ -2,27 +2,33 @@ dna_seq=open('data/data.fa.fna','r')
 output=open('results/output.txt','w')
 metiral=""
 Length=0
-GC_cont=0
-output.write("heder\tLength\tGC_amont\tCG_precent\n")
 
+output.write("heder\tLength\tGC_amont\tCG_precent\n")
+cont=0
 def rearol (metiral,heder):
+    GC_cont=0
     part=heder.split("[")
-    output.write(part[0]+"\t")
+    output.write(str(part[0])+"\t")
     Length=len(metiral)
-    output.write(Length+"\t")
+    output.write(str(Length)+"\t")
     for letter in metiral:
         if letter=="C" or letter=="G":
             GC_cont=GC_cont+1
-    output.write(GC_cont+"\t")
+    output.write(str(GC_cont)+"\t")
     GC_precent=GC_cont*100/Length
-    output.write(GC_precent+"\t")
+    output.write(("%.2f %%" % ((GC_precent)))+"\n")
 
 
 
 
 for line in dna_seq:
-    if line[0] !=">":
+    if cont==0:
+        curr_hedder=line
+        cont=cont+1
+    elif line[0] !=">":
         metiral=metiral+line.rstrip("\n\r")
     else:
-        rearol(metiral,line)
+        rearol(metiral,curr_hedder)
         metiral=""
+        curr_hedder=line
+rearol(metiral,curr_hedder)
