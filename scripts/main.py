0de8012a39f4fe4b88ba385f13a65bd90dcba605
FunChaosSeeker68 <thebet5575@gmail.com>
2026-05-18 09:12:15 +0300

    הוספתי פלט וגם דרך לחישוב והוצאה של כל מה שצריך לפלט

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,28 @@
 dna_seq=open('data/data.fa.fna','r')
+output=open('results/output.txt','w')
 metiral=""
-langh=0
+Length=0
 GC_cont=0
+output.write("heder\tLength\tGC_amont\tCG_precent\n")
+
+def rearol (metiral,heder):
+    part=heder.split("[")
+    output.write(part[0]+"\t")
+    Length=len(metiral)
+    output.write(Length+"\t")
+    for letter in metiral:
+        if letter=="C" or letter=="G":
+            GC_cont=GC_cont+1
+    output.write(GC_cont+"\t")
+    GC_precent=GC_cont*100/Length
+    output.write(GC_precent+"\t")
+
+
+
+
 for line in dna_seq:
     if line[0] !=">":
-        metiral=metiral+line.rstrip()
+        metiral=metiral+line.rstrip("\n\r")
     else:
-        langh=len(metiral)
-        for letter in metiral:
-            if letter=="C" or letter=="G":
-                GC_cont=GC_cont+1
+        rearol(metiral,line)
+        metiral=""
